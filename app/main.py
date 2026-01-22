353d7b3bfa9a0b04d3f4550defe21140cab61f38
Adler-Pereira <adlerlimap@gmail.com>
2026-01-21 23:20:51 -0300

    feat: add translation functions using deep_translator

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from json import JSONDecodeError
 from fastapi import FastAPI, HTTPException
 import requests
+from deep_translator import GoogleTranslator
 
 app = FastAPI()
+
+translator_pt = GoogleTranslator(source="en", target="pt")
+translator_en = GoogleTranslator(source="pt", target="en")
 
 def get_HTTPError(sts_code: int) -> str:
     match sts_code:
@@ -22,11 +26,28 @@ def get_HTTPError(sts_code: int) -> str:
             error = "Serviço indisponível"
         case _:
             error = "Erro HTTP"
+    return error
+
+def trans_text(text: str) -> str:
+    txt_trans = translator_en.translate(text)
+    return txt_trans
+
+def trans_dict(data: dict) -> dict:
+    meals = data.get("meals", [])
+    for meal in meals:
+        for key, value in meal.items():
+            if isinstance(value, str) and value.strip():
+                translated = translator_pt.translate(value)
+                meal[key] = translated
+    return {
+        "meals": meals
+    }
 
 @app.get("/recipe")
 def get_receita(ingredient: str) -> dict:
     url = "https://www.themealdb.com/api/json/v1/1/search.php"
-    params = {"s": ingredient}
+    ing_trans = trans_text(ingredient)
+    params = {"s": ing_trans}
     headers = {
         "User-Agent": "receitas-api-python",
         "Accept": "application/json"
@@ -42,13 +63,14 @@ def get_receita(ingredient: str) -> dict:
         response.raise_for_status()
         data = response.json()
 
-        if not data.get("meals"):
+        if not data.get("meals", []):
             raise HTTPException(
                 status_code = 404,
                 detail = "Nenhuma receita encontrada"
             )
         
-        return data
+        return trans_dict(data)
+
     except requests.exceptions.HTTPError as e:
         status_code = e.response.status_code if e.response else 500
         error = get_HTTPError(status_code)
